f189710fe8512d07f50f45daedd7a6463b8d320d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-11 12:23:10 +0000

    Deploy Gniar/zenaddress to github.com/Gniar/zenaddress.git:gh-pages

---
+++ precache-manifest.cd18c22cdf6474df5013a89f786421d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c978dd5940b2b9f86d564e6aac800286",
+    "url": "/zenaddress/index.html"
+  },
+  {
+    "revision": "b60f3abf707978245c73",
+    "url": "/zenaddress/static/css/main.23c58351.chunk.css"
+  },
+  {
+    "revision": "1ae3a2dd73d6d965e66b",
+    "url": "/zenaddress/static/js/2.758ee207.chunk.js"
+  },
+  {
+    "revision": "b60f3abf707978245c73",
+    "url": "/zenaddress/static/js/main.a2e4bba8.chunk.js"
+  },
+  {
+    "revision": "3ae5d7a5bf7e5531898c",
+    "url": "/zenaddress/static/js/runtime~main.43880aaa.js"
+  },
+  {
+    "revision": "fd7a2da22eb0e6015d847d675cbc99cd",
+    "url": "/zenaddress/static/media/bg.fd7a2da2.jpg"
+  },
+  {
+    "revision": "09e5ad836889f12c39cbcd991a4d0ca0",
+    "url": "/zenaddress/static/media/logo_full.09e5ad83.png"
+  },
+  {
+    "revision": "57e31348bdac66977c1473470b57e301",
+    "url": "/zenaddress/static/media/zen_paper_back.57e31348.png"
+  },
+  {
+    "revision": "b2f9fe3f6d4ea71a460c52e9421ae052",
+    "url": "/zenaddress/static/media/zen_paper_front.b2f9fe3f.png"
+  },
+  {
+    "revision": "16b33629a111fb18d64d9f6608b9ecab",
+    "url": "/zenaddress/static/media/zen_paper_sidebyside.16b33629.png"
+  }
+]);
